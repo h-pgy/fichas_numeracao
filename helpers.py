c6a25cb800f18b68543830fcb31b28a009136dc3
Joyce Silveira <57014508+JoyceSilveira@users.noreply.github.com>
2020-03-09 12:25:57 -0300

    add funcao para validar add

--- helpers.py
+++ helpers.py
@@ -1,3 +1,6 @@
+import json
+import requests
+
 # refaz a lista para tirar itens que estao repetidos> função futura
 def clean_dup_list(list_dup_items):
     unique_list = list()
@@ -5,3 +8,16 @@ def clean_dup_list(list_dup_items):
         if list_dup_items[i] not in list_dup_items[i + 1:]:
             unique_list.append(list_dup_items[i])
     return unique_list
+
+def validar_AD(user, passw):
+    url = 'http://10.75.18.36:8083/conexao_ldap/?user={user}&passw={passw}'.format(user=user, passw=passw)
+
+    # a linha abaixo pode virar uma função separada depois de resolução de proxy
+    proxies = {'http': 'http://{user}:{passw}@10.10.193.25:3128'.format(user=user, passw=passw),
+               'https': 'https://{user}:{passw}@10.10.193.25:3128'.format(user=user, passw=passw)}
+
+    # with requests.get(url, proxies=proxies) as f:
+    with requests.get(url) as f:
+        json_resp = f.text
+
+    return json.loads(json_resp)
