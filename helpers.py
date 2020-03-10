a647a75f74c8e43188a4976c478433e2417114e6
h-pgy <hpougy@gmail.com>
2020-03-10 12:14:50 -0300

    rodando AD obrigatoriamente dentro da intranet

--- helpers.py
+++ helpers.py
@@ -14,12 +14,9 @@ def clean_dup_list(list_dup_items):
 def validar_AD(user, passw):
     url = 'http://10.75.18.36:8083/conexao_ldap/?user={user}&passw={passw}'.format(user=user, passw=passw)
 
-    # a linha abaixo pode virar uma função separada depois de resolução de proxy
-    proxies = {'http': 'http://{user}:{passw}@10.10.193.25:3128'.format(user=user, passw=passw),
-               'https': 'https://{user}:{passw}@10.10.193.25:3128'.format(user=user, passw=passw)}
-
-    # with requests.get(url, proxies=proxies) as f:
-    with requests.get(url) as f:
+    s = requests.Session()
+    s.trust_env = False # obrigando a funcionar dentro da rede da PMSP
+    with s.get(url) as f:
         json_resp = f.text
 
     return json.loads(json_resp)
