a11314a47325347c47ab9bed9901e9d653cf59ac
h-pgy <hpougy@gmail.com>
2020-03-10 10:53:36 -0300

    pegar IP dinamicamente

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+import socket
 
 app = Flask(__name__)
 app.config.from_pyfile('app_config.py')
+IP = socket.gethostbyname(socket.gethostname())
 
 # importar as views somente após criar o app pois se tratam apenas das rotas do app e caso importado no começo da pagina
 # causaria um erro de "loop" pois o app é chamado dentro das views
@@ -10,6 +12,6 @@ from views import *
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='10.75.19.181', port='8086')
+    app.run(debug=True, host=IP, port='8086')
 
 
