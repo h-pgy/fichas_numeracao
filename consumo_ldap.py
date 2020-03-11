ccc8c4bf9ec3fd206fe229093a7a0403b8fde7c4
h-pgy <hpougy@gmail.com>
2020-03-11 11:47:35 -0300

    modulo para padronizar consumo do ldap

---
+++ consumo_ldap.py
@@ -0,0 +1,22 @@
+import requests
+
+LDAP_SERVICE  = {
+    'ip' : 'COLOCAR_O_IP',
+    'porta' : 'COLOCAR_A_PORTA'
+}
+
+
+def validar_ad(user, passw, ip_ldap, porta_ldap):
+
+    url = f'http://{ip_ldap}:{porta_ldap}/conexao_ldap'
+
+    s = requests.Session()
+    s.trust_env = False #garante que roda na intranet
+
+    with s.post(url, data = {'user' : user, 'passw' : passw}) as r:
+
+        resposta = r.json()
+        assert resposta['request_status'] == 'success'
+
+    return resposta['response']
+
